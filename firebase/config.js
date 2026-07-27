1021e7e31c8083d022368a990d651e992f3abecf
vkplkmr-dotcom <vkplkmr@gmail.com>
2026-07-27 17:38:38 +0530

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -4,29 +4,24 @@ const firebaseConfig = {
 
   apiKey: "AIzaSyAz6P7ZbM5zWiyvUypT_wF_gmj9jT-qwa0",
 
-  authDomain:
-  "coachsir-digital-student-card.firebaseapp.com",
+  authDomain: "coachsir-digital-student-card.firebaseapp.com",
 
-  projectId:
-  "coachsir-digital-student-card",
+  projectId: "coachsir-digital-student-card",
 
-  storageBucket:
-  "coachsir-digital-student-card.firebasestorage.app",
+  storageBucket: "coachsir-digital-student-card.firebasestorage.app",
 
-  messagingSenderId:
-  "953566380799",
+  messagingSenderId: "953566380799",
 
-  appId:
-  "1:953566380799:web:279a9ca2b19ab4d416c2ca"
+  appId: "1:953566380799:web:279a9ca2b19ab4d416c2ca"
 
 };
 
 
-// Firebase Start
+// Initialize Firebase
 
 firebase.initializeApp(firebaseConfig);
 
 
-// Firestore Database
+// Firestore
 
 const db = firebase.firestore();
